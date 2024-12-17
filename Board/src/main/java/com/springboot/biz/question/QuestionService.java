490b91f91fda0ece8b16c167d906b97e907726fe
jinsaum <i3295h@gamil.com>
2024-12-17 14:27:43 +0900

    페이지 네이션 완성

--- Board/src/main/java/com/springboot/biz/question/QuestionService.java
+++ Board/src/main/java/com/springboot/biz/question/QuestionService.java
@@ -1,12 +1,14 @@
 package com.springboot.biz.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.springboot.biz.DataNotFoundException;
@@ -27,7 +29,9 @@ public class QuestionService { //INSERT, SELECT, UPDATE, DELETE 등의 역할
 	}
 	
 	public Page<Question> getList(int page){
-		Pageable pageable = PageRequest.of(page, 10);
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("createDate"));
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
 		return this.questionRepository.findAll(pageable);
 	}
 	
