7f55c5c21f762c33058bb3dd5ad6de7cdad999c7
jinsaum <i3295h@gamil.com>
2024-12-19 13:15:48 +0900

    로그인 기능 구현

--- Board/src/main/java/com/springboot/biz/SecurityConfig.java
+++ Board/src/main/java/com/springboot/biz/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.springboot.biz;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -40,5 +42,10 @@ public class SecurityConfig {
 	 return new BCryptPasswordEncoder();
  }
  
+ @Bean
+ AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+	 return authenticationConfiguration.getAuthenticationManager();
+ }
  
+
 }
